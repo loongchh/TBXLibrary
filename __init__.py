16828cc54c19bb569ac65760f47e8f907d1e133d
Long-Huei Chen <longhuei@stanford.edu>
2018-09-20 11:55:05 +0900

    Update license text

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+"""__init__.py
+
+Written in 2018 by Long-Huei Chen <longhuei@g.ecc.u-tokyo.ac.jp>
+
+To the extent possible under law, the author(s) have dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with
+this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+"""
+
 from .tbxlibrary import tbx2dict
 
 __all__ = ["tbx2dict"]
